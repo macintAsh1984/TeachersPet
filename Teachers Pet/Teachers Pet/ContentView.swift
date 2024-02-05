98cdbf278c56a4724b1fd2186838d4c74933aa14
macintAsh1984 <amvaldez@ucdavis.edu>
2024-02-05 08:01:45 -0800

    Created project files.

---
+++ Teachers Pet/Teachers Pet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Teachers Pet
+//
+//  Created by Ashley Valdez on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Teachers Pet/Teachers Pet/Teachers_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teachers_PetApp.swift
+//  Teachers Pet
+//
+//  Created by Ashley Valdez on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Teachers_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
