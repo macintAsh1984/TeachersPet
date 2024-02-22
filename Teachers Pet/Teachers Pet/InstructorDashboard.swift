0778def6a63d3e7a8dc4e2a05bbebf5a8d771226
macintAsh1984 <ashh.valdez@gmail.com>
2024-02-22 00:03:34 -0800

    [Issue #7] Implemented logic for creating a QR code that contains class invite code.

---
+++ Teachers Pet/Teachers Pet/InstructorDashboard.swift
@@ -0,0 +1,18 @@
+//
+//  InstructorDashboard.swift
+//  Teachers Pet
+//
+//  Created by Ashley Valdez on 2/21/24.
+//
+
+import SwiftUI
+
+struct InstructorDashboard: View {
+    var body: some View {
+        Text("Instructor Dashboard")
+    }
+}
+
+#Preview {
+    InstructorDashboard()
+}
