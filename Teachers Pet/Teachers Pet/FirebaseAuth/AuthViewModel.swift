08b757ef9c41ba5b1c1afbfe0b1a635f01be71c9
macintAsh1984 <ashh.valdez@gmail.com>
2024-02-27 17:17:04 -0800

    Added missing view model files for Firebase.

---
+++ Teachers Pet/Teachers Pet/FirebaseAuth/AuthViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  AuthViewModel.swift
+//  Teachers Pet
+//
+//  Created by Sukhpreet Aulakh on 2/25/24.
+//
+
+//This file will be responsible for having all functionality to updating the backend with user
+//for instance, email and password.
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+
+@MainActor
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
+    
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
+    }
+    
+    func signIn(withEmail email: String, password: String) async throws {
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            await fetchUser()
+        } catch {
+            print("Failed to log in \(error.localizedDescription)")
+        }
+    }
+    
+    func createUser(withEmail email: String, password: String, fullname: String, coursename: String, joincode: String) async throws {
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password) //create user using firebase code that we installed
+            self.userSession = result.user //if we get successful result back
+            let user = User(id: result.user.uid, fullname: fullname, email: email, coursename: coursename, joincode: joincode) //create our user object, from Model itself
+            let encodedUser = try Firestore.Encoder().encode(user) //encode that object through the encodable protocol, encodes into JSON data so that it can be stored into firebase
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser) //upload data to firestore
+            await fetchUser()
+        } catch {
+            print("Failed to create user \(error.localizedDescription)")
+        }
+    }
+    
+    func signout() {
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+            self.currentUser = nil
+        } catch {
+            print("Failed to sign out")
+        }
+    }
+    
+    
+    func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else {return }
+        self.currentUser = try? snapshot.data(as: User.self)
+        
+        print("Current user is \(self.currentUser)")
+        
+    }
+}
