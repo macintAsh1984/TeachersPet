bb6a032cc84e434f601eabff748c5d610f7708ab
macintAsh1984 <ashh.valdez@gmail.com>
2024-03-01 11:18:20 -0800

    [Issue #19] Created UI pages for showing student line positions and showing the overall office hours line.

---
+++ Teachers Pet/Teachers Pet/InstructorView/OHLineManagement.swift
@@ -0,0 +1,67 @@
+//
+//  OHLineManagement.swift
+//  Teachers Pet
+//
+//  Created by Ashley Valdez on 3/1/24.
+//
+
+import SwiftUI
+
+struct OHLineManagement: View {
+    @State var students = [
+        "Student A",
+        "Student B",
+        "Student C",
+        "Student D"
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(0..<students.count, id: \.self) { index in
+                        StudentEntry(studentInLine: students[index])
+                  }
+                    .onDelete { indexSet in
+                        students.remove(atOffsets: indexSet)
+                    }
+                    .onMove { indices, newOffset in
+                        students.move(fromOffsets: indices, toOffset: newOffset)
+                    }
+                }
+                .navigationTitle("My Office Hours")
+                .navigationBarItems(trailing: EditButton())
+
+            }
+        }
+    }
+}
+
+
+struct StudentEntry: View {
+    @State var studentInLine: String
+    
+    var body: some View {
+            NavigationStack {
+                  HStack {
+                      Image(systemName: "graduationcap")
+                      .resizable()
+                      .aspectRatio(contentMode: .fill)
+                      .frame(width: 30, height: 30)
+                      .cornerRadius(5)
+                      .padding(.leading, 8)
+                    Text(studentInLine)
+                      .font(.headline)
+                      .lineLimit(1)
+                    Spacer()
+                  }
+                  .padding(.vertical, 8)
+                }
+
+    }
+}
+
+
+#Preview {
+    OHLineManagement()
+}
---
+++ Teachers Pet/Teachers Pet/StudentView/OHLineView.swift
@@ -0,0 +1,62 @@
+//
+//  OHLineView.swift
+//  Teachers Pet
+//
+//  Created by Ashley Valdez on 2/29/24.
+//
+
+import SwiftUI
+
+struct OHLineView: View {
+    @State var leaveLineAlert = false
+    @State var returnToStudentDashboard = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("You are #3 in queue")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                
+                
+                Button {
+                    leaveLineAlert = true
+                } label: {
+                    Text("Leave Office Hours Line")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.orange)
+                .controlSize(.large)
+                Spacer()
+            }
+            .padding()
+            .navigationBarBackButtonHidden()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .preferredColorScheme(.light)
+            .background(Color("AppBackgroundColor"))
+            .alert(isPresented: $leaveLineAlert) {
+                Alert(
+                    title: Text("Are you sure you want to leave the line?"),
+                    message: Text("This cannot be undone."),
+                    primaryButton: .destructive(Text("Leave")) {
+                        //leave line
+                        returnToStudentDashboard = true
+                    },
+                    secondaryButton: .cancel()
+                    
+                )
+            }
+            .navigationDestination(isPresented: $returnToStudentDashboard) {
+                StudentDashboard()
+            }
+        }
+    }
+}
+
+#Preview {
+    OHLineView()
+}
