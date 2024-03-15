14038041f9d99addb52998a9f98d09359043602f
macintAsh1984 <ashh.valdez@gmail.com>
2024-03-14 20:17:56 -0700

    [Issue #23] Refactored Create Class page..

--- Teachers Pet/Teachers Pet/InstructorView/CreateClass.swift
+++ Teachers Pet/Teachers Pet/InstructorView/CreateClass.swift
@@ -8,67 +8,107 @@
 import SwiftUI
 
 struct CreateClass: View {
-    @EnvironmentObject var viewModel: AuthViewModel
-    @State var courseName = String()
-    @State var navigateToCodeGeneration = false
-    @State var navgiateToJoinClass = false
+    //User Account Info Binding/State Variables
     @Binding var email: String
     @Binding var password: String
     @Binding var Name: String
-
+    @State var courseName = String()
+    
+    //Navigation Toggle State Variables
+    @State var navigateToCodeGeneration = false
+    @State var navigateToJoinClass = false
+    @State var noCourseNameAlert = false
+    
+    @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View {
         NavigationStack {
-            VStack (alignment: .leading) {
-                Text("Create A Class")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
+            VStack {
                 Spacer()
-                Text("Enter Course Name")
-                    .font(.title2)
-                    .fontWeight(.semibold)
+                CreateClassPageTitle()
                 TextField("Course Name", text: $courseName)
                     .padding(.all)
                     .background()
                     .cornerRadius(10.0)
                 Spacer()
-                Button {
-                    navigateToCodeGeneration = true
-                } label: {
-                    Text("Create Class")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.orange)
-                .controlSize(.large)
+                    .frame(height: 40)
+                CreateClassButton(navigateToCodeGeneration: $navigateToCodeGeneration, courseName: $courseName, noCourseNameAlert: $noCourseNameAlert)
                 Spacer()
-                
-                HStack(spacing: 5) {
-                    Text("Have a class join code?")
-                    Button {
-                        navgiateToJoinClass = true
-                    } label: {
-                        Text("Join Your Class")
-                            .underline()
-                            .foregroundStyle(.orange)
-                    }
-    
-                }
+                    .frame(height: 40)
+                JoinClassPageButton(navigateToJoinClass: $navigateToJoinClass)
+                Spacer()
             }
             .padding()
             .preferredColorScheme(.light)
             .background(appBackgroundColor)
+            .alert(isPresented: $noCourseNameAlert) {
+                Alert(title: Text("Invalid Course Name"), message: Text("Please Enter A Course Name"), dismissButton: .cancel(Text("OK")))
+            }
+
             .navigationDestination(isPresented: $navigateToCodeGeneration) {
                 ClassJoinCodeGeneration(email: $email, password: $password, Name: $Name, coursename: $courseName)
             }
-            .navigationDestination(isPresented: $navgiateToJoinClass) {
+            .navigationDestination(isPresented: $navigateToJoinClass) {
                 InstructorJoinClass(email: $email, password: $password, name: $Name)
             }
         }
         
+    }
+}
+
+struct CreateClassPageTitle: View {
+    var body: some View {
+        Text("Create A Class")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+        Spacer()
+            .frame(height: 60)
+        Text("Enter Course Name")
+            .font(.title2)
+            .fontWeight(.medium)
+    }
+}
+
+struct CreateClassButton: View {
+    @Binding var navigateToCodeGeneration: Bool
+    @Binding var courseName: String
+    @Binding var noCourseNameAlert: Bool
+    
+    var body: some View {
+        Button {
+            if courseName.isEmpty {
+                noCourseNameAlert = true
+            } else {
+                navigateToCodeGeneration = true
+            }
+        } label: {
+            Text("Create Class")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.green)
+        .controlSize(.large)
+    }
+}
+
+struct JoinClassPageButton: View {
+    @Binding var navigateToJoinClass: Bool
+    
+    var body: some View {
+        HStack(spacing: 5) {
+            Text("Have a class join code?")
+            Button {
+                navigateToJoinClass = true
+            } label: {
+                Text("Join Your Class")
+                    .underline()
+                    .foregroundStyle(.green)
+            }
+
+        }
     }
 }
 
